affb1e5001096f1f3ce468037a4928f7980f0078
DavidEGx <DavidEGx@users.noreply.github.com>
2023-11-09 08:32:55 +0000

    ✅ Ready to clone and code.

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from testxxxx.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "testxxxx"
--- testxxxx/cli.py
+++ testxxxx/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for testxxxx project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m testxxxx` and `$ testxxxx `.
 
     This is your program's entry point.
 
